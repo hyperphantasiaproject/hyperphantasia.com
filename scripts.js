90f89428632c6ffc8563772914b474f82f261a56
Hyperphantasia Project <58660866+hyperphantasiaproject@users.noreply.github.com>
2025-12-20 14:30:22 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// -----------------------------
+// Background music (play on first click)
+// -----------------------------
+
+document.addEventListener("click", function () {
+    document.getElementById("bgm").play();
+    const audio = document.getElementById("bgm");
+  audio.volume = 0.05; // 5% volume
+  }, { once: true });
+
+// -----------------------------
+// Clock (HH:MM with blink + fade)
+// -----------------------------
+
+function updateClock() {
+  const now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+
+  if (h < 10) h = "0" + h;
+  if (m < 10) m = "0" + m;
+
+  document.getElementById("clock").textContent =
+    h + ":" + m;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
